0f6826d1f3595d252cc5f2655a9fc3f191096cb1
Théo Bori <theo1.bori@epitech.eu>
2024-10-30 22:15:42 +0100

    ✨ (thunderbird): Added an accounts options with a default value

--- modules/home/messages/thunderbird/default.nix
+++ modules/home/messages/thunderbird/default.nix
@@ -7,16 +7,41 @@
 let
   inherit (lib) mkIf types;
   inherit (lib.${namespace}) mkBoolOpt mkOpt;
+  inherit (config.${namespace}) user;
 
   cfg = config.${namespace}.messages.thunderbird;
 in
 {
   options.${namespace}.messages.thunderbird = with types; {
     enable = mkBoolOpt false "Whether or not to manage thunderbird.";
-    name = mkOpt str config.${namespace}.user.name "The thunderbird default profile name";
+    name = mkOpt str user.name "The thunderbird default profile name";
+    # TODO: Create an other module to manage email accounts, this module should only add the `thunderbird` key within each email accounts
+    # For my use case, it works well because I only use Thunderbird as email client.
+    accounts = mkOpt attrs {
+      "tilde.team" = {
+        realName = user.fullName;
+        address = "nagi@tilde.team";
+        userName = "nagi";
+        primary = true;
+        smtp = {
+          host = "smtp.tilde.team";
+          port = 465;
+          tls = {
+            enable = true;
+            useStartTls = true;
+          };
+        };
+        thunderbird = {
+          enable = true;
+          profiles = [ user.name ];
+        };
+      };
+    } "The thunderbird email accounts";
   };
 
   config = mkIf cfg.enable {
+    accounts.email.accounts = cfg.accounts;
+
     programs.thunderbird = {
       enable = true;
 
