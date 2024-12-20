3a27fb5948f912e466b1e4a64a4a447d908ba1d2
Théo Bori <theo1.bori@epitech.eu>
2024-12-20 16:49:21 +0100

    :sparkles: Added a new system `rob`

---
+++ homes/x86_64-linux/theobori@rob/default.nix
@@ -0,0 +1,34 @@
+{ lib, namespace, ... }:
+let
+  inherit (lib.${namespace}) enabled;
+in
+{
+  theobori-nix = {
+    roles = {
+      social = enabled;
+      desktop = enabled;
+      gaming = enabled;
+    };
+
+    user = {
+      enable = true;
+      name = "theobori";
+    };
+
+    cli.programs = {
+      ssh.useSops = true;
+      gpg.useSops = true;
+    };
+
+    services = {
+      sops = {
+        enable = true;
+        defaultSopsFile = lib.snowfall.fs.get-file "secrets/rob/theobori/secrets.yaml";
+      };
+    };
+
+    desktops.addons.plasma6 = enabled;
+  };
+
+  home.stateVersion = "24.11";
+}
---
+++ systems/x86_64-linux/rob/disk-config.nix
@@ -0,0 +1,61 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              name = "boot";
+              size = "1M";
+              type = "EF02";
+            };
+            esp = {
+              name = "ESP";
+              size = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                extraOpenArgs = [ ];
+                settings = {
+                  allowDiscards = true;
+                };
+                content = {
+                  type = "lvm_pv";
+                  vg = "pool";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [ "defaults" ];
+            };
+          };
+        };
+      };
+    };
+  };
+}
