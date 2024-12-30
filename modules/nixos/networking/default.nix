63417d7a8552fcc58d6f55636eb544de11fecacd
Théo Bori <theo1.bori@epitech.eu>
2024-12-30 21:34:27 +0100

    :sparkles: Added Steven Black's hosts via its Nix module

---
+++ modules/nixos/networking/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  config,
+  namespace,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt;
+
+  cfg = config.${namespace}.networking.stevenBlackHosts;
+in
+{
+  options.${namespace}.networking.stevenBlackHosts = {
+    enable = mkBoolOpt false "Whether or not to enable the Steven Black's hosts.";
+  };
+
+  config = mkIf cfg.enable {
+    networking.stevenBlackHosts = {
+      enable = true;
+      blockFakenews = true;
+      blockGambling = true;
+      blockPorn = true;
+      blockSocial = true;
+    };
+  };
+}
