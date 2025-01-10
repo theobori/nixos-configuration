4bf7490629bfe46f0a94dff77e6b38943b1b426a
Théo Bori <theo1.bori@epitech.eu>
2025-01-10 20:45:40 +0100

    :sparkles: Inserted nameservers via networkmanager

--- modules/nixos/hardware/networking/default.nix
+++ modules/nixos/hardware/networking/default.nix
@@ -5,33 +5,47 @@
   ...
 }:
 let
-  inherit (lib) mkIf;
-  inherit (lib.${namespace}) mkBoolOpt enabled;
+  inherit (lib) mkIf types;
+  inherit (lib.${namespace}) mkBoolOpt mkOpt;
 
   cfg = config.${namespace}.hardware.networking;
 in
 {
-  options.${namespace}.hardware.networking = {
+  options.${namespace}.hardware.networking = with types; {
     enable = mkBoolOpt false "Enable networkmanager.";
+    insertNameservers = mkOpt (listOf str) [
+      # Cloudflare
+      "1.1.1.1"
+      "1.0.0.1"
+      # Google
+      "8.8.8.8"
+      "8.8.4.4"
+    ] "Insert nameservers before the ones added by networkmanager";
   };
 
   config = mkIf cfg.enable {
-    networking.firewall = {
-      enable = true;
-      allowedTCPPortRanges = [
-        {
-          from = 1714;
-          to = 1764;
-        }
-      ];
-      allowedUDPPortRanges = [
-        {
-          from = 1714;
-          to = 1764;
-        }
-      ];
-    };
+    networking = {
+      networkmanager = {
+        enable = true;
 
-    networking.networkmanager = enabled;
+        inherit (cfg) insertNameservers;
+      };
+
+      firewall = {
+        enable = true;
+        allowedTCPPortRanges = [
+          {
+            from = 1714;
+            to = 1764;
+          }
+        ];
+        allowedUDPPortRanges = [
+          {
+            from = 1714;
+            to = 1764;
+          }
+        ];
+      };
+    };
   };
 }
