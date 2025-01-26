2e467c7cf818da60da857d2d21b394ba05889c92
Théo Bori <theo1.bori@epitech.eu>
2025-01-26 01:05:53 +0100

    :package: Added the `ttv-lol-pro` firefox addon

--- modules/home/browsers/firefox/default.nix
+++ modules/home/browsers/firefox/default.nix
@@ -74,6 +74,24 @@ in
           };
         }
       )
+      (
+        let
+          version = "2.3.10";
+        in
+        buildFirefoxXpiAddon {
+          pname = "ttv-lol-pro";
+          inherit version;
+          addonId = "{76ef94a4-e3d0-4c6f-961a-d38a429a332b}";
+          url = "https://addons.mozilla.org/firefox/downloads/file/4357094/ttv_lol_pro-${version}.xpi";
+          sha256 = "sha256-szzQzU41IVIO8jOyEXkij9reyhaDWayrTCS7saFm/No=";
+          meta = {
+            homepage = "";
+            description = "";
+            license = lib.licenses.asl20;
+            platforms = lib.platforms.all;
+          };
+        }
+      )
     ]) "Extensions to install.";
 
     policies = mkOpt attrs {
