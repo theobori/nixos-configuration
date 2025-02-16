1f99816101b7b8a48bb466a92185a13a8d3ec8b8
Théo Bori <theo1.bori@epitech.eu>
2025-02-16 02:11:48 +0100

    :package: Added a LSP server for the Go language

--- modules/home/editors/emacs/default.nix
+++ modules/home/editors/emacs/default.nix
@@ -16,13 +16,20 @@ in
     enable = mkBoolOpt false "Whether or not to enable emacs.";
 
     extraConfig = mkOpt str ''
-      (global-auto-revert-mode 1)
+            (global-auto-revert-mode 1)
 
-      (menu-bar-mode -1)
-      (global-display-line-numbers-mode)
-      (setq standard-indent 2)
-      (setq font-lock-maximum-decoration t)
-      (setq display-line-numbers-type 'relative)
+            (menu-bar-mode -1)
+            (global-display-line-numbers-mode)
+            (setq standard-indent 2)
+            (setq font-lock-maximum-decoration t)
+            (setq display-line-numbers-type 'relative)
+
+            ;; See https://snarfed.org/gnu_emacs_backup_files
+            (custom-set-variables
+        '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+        '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+
+      (make-directory "~/.emacs.d/autosaves/" t)
     '' "Emacs extra configuration.";
   };
 
