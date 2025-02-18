28a054bc4bcbe5fac569f8102a9e48d396c2d645
Théo Bori <theo1.bori@epitech.eu>
2025-02-18 20:19:56 +0100

    :package: Added the `doom-modeline` GNU Emacs package

---
+++ modules/home/editors/emacs/packages/doom-modeline/default.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt;
+
+  cfg = config.${namespace}.editors.emacs.packages.doom-modeline;
+in
+{
+  options.${namespace}.editors.emacs.packages.doom-modeline = {
+    enable = mkBoolOpt false "Whether or not to enable the emacs doom-modeline package.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.emacs = {
+      extraPackages = (
+        epkgs: [
+          epkgs.doom-modeline
+          epkgs.all-the-icons
+        ]
+      );
+      extraConfig = ''
+        (use-package all-the-icons)
+        (use-package doom-modeline
+          :ensure t
+          :init (doom-modeline-mode 1))
+      '';
+    };
+  };
+}
