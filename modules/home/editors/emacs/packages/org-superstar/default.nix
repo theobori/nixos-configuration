adc4ccad77efad3dd8dfc4e330e52469e837a93d
Théo Bori <theo1.bori@epitech.eu>
2025-02-18 21:31:51 +0100

    :package: Added the `org-superstar` GNU Emacs package

---
+++ modules/home/editors/emacs/packages/org-superstar/default.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt;
+
+  cfg = config.${namespace}.editors.emacs.packages.org-superstar;
+in
+{
+  options.${namespace}.editors.emacs.packages.org-superstar = {
+    enable = mkBoolOpt false "Whether or not to enable the emacs org-superstar package.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.emacs = {
+      extraPackages = (epkgs: [ epkgs.org-superstar ]);
+      extraConfig = ''
+        (use-package org-superstar
+          :ensure t
+          :after org
+          :hook (org-mode . org-superstar-mode)
+          :custom
+            (org-superstar-remove-leading-stars t)
+            (org-superstar-headline-bullets-list '("⁖" "✿" "▷" "✸")))
+      '';
+    };
+  };
+}
