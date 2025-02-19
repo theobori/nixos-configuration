e0cd046b5cb85ca0a1a3ca4adb07d02ecc19b72d
Théo Bori <theo1.bori@epitech.eu>
2025-02-19 20:50:03 +0100

    :package: Added the `dashboard` GNU Emacs package

---
+++ modules/home/editors/emacs/packages/dashboard/default.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt;
+
+  cfg = config.${namespace}.editors.emacs.packages.dashboard;
+in
+{
+  options.${namespace}.editors.emacs.packages.dashboard = {
+    enable = mkBoolOpt false "Whether or not to enable the emacs Dashboard package.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.emacs = {
+      extraPackages = (epkgs: [ epkgs.dashboard ]);
+      extraConfig = ''
+        (use-package dashboard
+          :ensure t
+          :config
+          (dashboard-setup-startup-hook)
+          (setq dashboard-startup-banner 'official
+                dashboard-center-content t
+                dashboard-items '((projects . 5)
+                                   (recents  . 5)))
+          (setq dashboard-set-footer nil))
+
+          (setq dashboard-banner-logo-title "This is your life")
+          (setq dashboard-set-file-icons t)
+          (setq dashboard-projects-backend 'project-el)
+
+          (setq initial-buffer-choice (lambda ()
+                                          (get-buffer-create "*dashboard*")
+                                          (dashboard-refresh-buffer)))
+      '';
+    };
+  };
+}
