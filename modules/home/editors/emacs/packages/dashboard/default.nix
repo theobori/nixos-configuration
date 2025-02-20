f11f80d6c86f83cc401d7e969958d6c4bbf406e9
Théo Bori <theo1.bori@epitech.eu>
2025-02-20 13:04:27 +0100

    :bug: Open the dashboard only with no CLi args

--- modules/home/editors/emacs/packages/dashboard/default.nix
+++ modules/home/editors/emacs/packages/dashboard/default.nix
@@ -7,14 +7,12 @@
 let
   inherit (lib) mkIf;
   inherit (lib.${namespace}) mkBoolOpt;
-
   cfg = config.${namespace}.editors.emacs.packages.dashboard;
 in
 {
   options.${namespace}.editors.emacs.packages.dashboard = {
     enable = mkBoolOpt false "Whether or not to enable the emacs Dashboard package.";
   };
-
   config = mkIf cfg.enable {
     programs.emacs = {
       extraPackages = (epkgs: [ epkgs.dashboard ]);
@@ -22,20 +20,21 @@ in
         (use-package dashboard
           :ensure t
           :config
-          (dashboard-setup-startup-hook)
-          (setq dashboard-startup-banner 'official
-                dashboard-center-content t
-                dashboard-items '((projects . 5)
-                                   (recents  . 5)))
-          (setq dashboard-set-footer nil))
-
-          (setq dashboard-banner-logo-title "This is your life")
-          (setq dashboard-set-file-icons t)
-          (setq dashboard-projects-backend 'project-el)
-
-          (setq initial-buffer-choice (lambda ()
-                                          (get-buffer-create "*dashboard*")
-                                          (dashboard-refresh-buffer)))
+          (when (and (not (daemonp))
+                    (not window-system)
+                    (= (length command-line-args) 1))
+            (dashboard-setup-startup-hook)
+            (setq dashboard-startup-banner 'official
+                  dashboard-center-content t
+                  dashboard-items '((projects . 5)
+                                  (recents  . 5)))
+            (setq dashboard-set-footer nil)
+            (setq dashboard-banner-logo-title "This is your life")
+            (setq dashboard-set-file-icons t)
+            (setq dashboard-projects-backend 'project-el)
+            (setq initial-buffer-choice (lambda ()
+                                        (get-buffer-create "*dashboard*")
+                                        (dashboard-refresh-buffer)))))
       '';
     };
   };
