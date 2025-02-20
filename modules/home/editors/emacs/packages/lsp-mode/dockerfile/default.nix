f5e656087ffdef7dbdc36fb1c08781616268f236
Théo Bori <theo1.bori@epitech.eu>
2025-02-20 15:56:03 +0100

    :package: Added a Dockerfiles LSP for GNU Emacs

---
+++ modules/home/editors/emacs/packages/lsp-mode/dockerfile/default.nix
@@ -0,0 +1,46 @@
+{
+  config,
+  lib,
+  namespace,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt;
+  cfg = config.${namespace}.editors.emacs.packages.lsp-mode.dockerfile;
+  lsp-mode = config.${namespace}.editors.emacs.packages.lsp-mode;
+in
+{
+  options.${namespace}.editors.emacs.packages.lsp-mode.dockerfile = {
+    enable = mkBoolOpt false "Whether or not to enable dockerfile for lsp-mode (Emacs).";
+  };
+
+  config = mkIf (lsp-mode.enable && cfg.enable) {
+    home.packages = with pkgs; [ dockerfile-language-server-nodejs ];
+
+    programs.emacs = {
+      extraPackages = (epkgs: [ epkgs.dockerfile-mode ]);
+      extraConfig = ''
+         (use-package dockerfile-mode
+           :after lsp-mode
+           :ensure t
+           :hook
+           (nix-mode . lsp-deferred) ;; So that envrc mode will work
+           :mode "\\Dockerfile?$"
+           :config
+             (put 'dockerfile-image-name 'safe-local-variable #'stringp))
+
+        (with-eval-after-load 'lsp-mode
+          (add-to-list 'lsp-language-id-configuration '(dockerfile-mode . "dockerfile"))
+
+          (lsp-register-client (make-lsp-client
+            :new-connection (lsp-stdio-connection '("docker-langserver" "--stdio"))
+            :priority 0
+            :server-id 'dockerfile))
+
+          (add-hook 'dockerfile-mode-hook #'lsp))
+      '';
+    };
+  };
+}
