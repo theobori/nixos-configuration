02554168204f579c2d141ee0c2b27257943c2d70
Théo Bori <theo1.bori@epitech.eu>
2025-02-20 16:19:57 +0100

    :package: Added the `yaml-mode` package for GNU Emacs

---
+++ modules/home/editors/emacs/packages/yaml/default.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt;
+
+  cfg = config.${namespace}.editors.emacs.packages.yaml;
+in
+{
+  options.${namespace}.editors.emacs.packages.yaml = {
+    enable = mkBoolOpt false "Whether or not to enable the emacs Yaml package.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.emacs = {
+      extraPackages = (epkgs: [ epkgs.yaml-mode ]);
+      extraConfig = ''
+        (use-package yaml-mode
+          :commands (markdown-mode gfm-mode)
+          :mode (
+            ("\\.yml\\'" . yaml-mode)
+            ("\\.yaml\\'" . yaml-mode)))
+      '';
+    };
+  };
+}
