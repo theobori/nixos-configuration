7b9eb5356c31c8bfd149aeacbc1f82ae30dffd1d
Théo Bori <theo1.bori@epitech.eu>
2025-02-23 18:50:17 +0100

    :sparkles: Added a Firefox bookmark

--- modules/home/editors/emacs/default.nix
+++ modules/home/editors/emacs/default.nix
@@ -17,22 +17,24 @@ in
     package = mkOpt package pkgs.emacs "Whether or not to enable emacs.";
 
     extraConfig = mkOpt str ''
-            (global-auto-revert-mode 1)
-            (show-paren-mode t)
-            (defalias 'yes-or-no-p 'y-or-n-p)
+      (global-auto-revert-mode 1)
+      (show-paren-mode t)
+      (defalias 'yes-or-no-p 'y-or-n-p)
 
-            (menu-bar-mode -1)
-            (global-display-line-numbers-mode)
-            (setq standard-indent 2)
-            (setq font-lock-maximum-decoration t)
-            (setq display-line-numbers-type 'relative)
+      (menu-bar-mode -1)
+      (global-display-line-numbers-mode)
+      (setq standard-indent 2)
+      (setq font-lock-maximum-decoration t)
+      (setq display-line-numbers-type 'relative)
 
-            ;; See https://snarfed.org/gnu_emacs_backup_files
-            (custom-set-variables
+      ;; See https://snarfed.org/gnu_emacs_backup_files
+      (custom-set-variables
         '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
         '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
 
       (make-directory "~/.emacs.d/autosaves/" t)
+
+      (setq org-link-file-path-type 'relative)
     '' "Emacs extra configuration.";
   };
 
