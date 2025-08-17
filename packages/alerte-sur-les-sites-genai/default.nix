edecadd862105607e812342f8e5e744a9b19931b
Théo Bori <theo1.bori@epitech.eu>
2025-08-17 12:01:49 +0200

    :arrow_up: Updated the `alerte-sur-les-sites-genai` Firefox addon Nix derivation

--- packages/alerte-sur-les-sites-genai/default.nix
+++ packages/alerte-sur-les-sites-genai/default.nix
@@ -1,7 +1,7 @@
 { lib, pkgs, ... }:
 let
   inherit (pkgs.nur.repos.rycee) firefox-addons;
-  version = "2.2.1";
+  version = "2.3";
 in
 firefox-addons.buildFirefoxXpiAddon {
   pname = "alerte-sur-les-sites-genai";
@@ -10,7 +10,7 @@ firefox-addons.buildFirefoxXpiAddon {
   url = "https://addons.mozilla.org/firefox/downloads/file/4510278/alerte_sur_les_sites_genai-${version}.xpi";
   sha256 = "sha256-rHusk8LEQs5tB+TPgKi/5kjZ3FvtnFQ470JCFypPCUs=";
   meta = {
-    homepage = "";
+    homepage = "https://next.ink";
     description = "Displays an alert for sites generated in whole or in part by AI.";
     license = lib.licenses.asl20;
     platforms = lib.platforms.all;
