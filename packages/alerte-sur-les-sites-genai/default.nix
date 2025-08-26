26723dcb962823a326521406351329bd07735bd4
Théo Bori <theo1.bori@epitech.eu>
2025-08-26 22:45:38 +0200

    :bug: Fixed alerte sur les sites genia Firefox addon URL

--- packages/alerte-sur-les-sites-genai/default.nix
+++ packages/alerte-sur-les-sites-genai/default.nix
@@ -7,8 +7,8 @@ firefox-addons.buildFirefoxXpiAddon {
   pname = "alerte-sur-les-sites-genai";
   inherit version;
   addonId = "sebastien@gavois.fr";
-  url = "https://addons.mozilla.org/firefox/downloads/file/4510278/alerte_sur_les_sites_genai-${version}.xpi";
-  sha256 = "sha256-rHusk8LEQs5tB+TPgKi/5kjZ3FvtnFQ470JCFypPCUs=";
+  url = "https://addons.mozilla.org/firefox/downloads/file/4556934/alerte_sur_les_sites_genai-${version}.xpi";
+  sha256 = "sha256-5TDgQScsX9E6Jy9NlhC0l8OVHKCdyUxvwZ4n2z6VmOk=";
   meta = {
     homepage = "https://next.ink";
     description = "Displays an alert for sites generated in whole or in part by AI.";
