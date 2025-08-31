3d9fbfaf9dba76852d6b31fca189d30b44149eba
Théo Bori <theo1.bori@epitech.eu>
2025-09-01 00:30:04 +0200

    :zap: Better Lix integration

--- modules/home/services/lorri/default.nix
+++ modules/home/services/lorri/default.nix
@@ -6,20 +6,22 @@
   ...
 }:
 let
-  inherit (lib) mkIf;
-  inherit (lib.${namespace}) mkBoolOpt;
+  inherit (lib) mkIf types;
+  inherit (lib.${namespace}) mkBoolOpt mkOpt;
 
   cfg = config.${namespace}.services.lorri;
 in
 {
-  options.${namespace}.services.lorri = {
+  options.${namespace}.services.lorri = with types; {
     enable = mkBoolOpt false "Whether to enable lorri.";
+    nixPackage = mkOpt package pkgs.lixPackageSets.stable.lix "Which nix package to use.";
+
   };
 
   config = mkIf cfg.enable {
     services.lorri = {
       enable = true;
-      nixPackage = pkgs.lix;
+      inherit (cfg) nixPackage;
     };
   };
 }
