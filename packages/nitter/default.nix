f287b2d1ac73ddeb47f68388980aee8ddaf14c39
Théo Bori <theo1.bori@epitech.eu>
2025-09-09 17:50:55 +0200

    :package: Added the `nitter` Firefox addon

---
+++ packages/nitter/default.nix
@@ -0,0 +1,18 @@
+{ lib, pkgs, ... }:
+let
+  inherit (pkgs.nur.repos.rycee) firefox-addons;
+  version = "3.8";
+in
+firefox-addons.buildFirefoxXpiAddon {
+  pname = "nitter";
+  inherit version;
+  addonId = "{7b74340a-30bf-4a45-aefa-8a0de3096062}";
+  url = "https://addons.mozilla.org/firefox/downloads/file/4355534/nitter-${version}.xpi";
+  sha256 = "sha256-wdyYgV9qgktStkpFob6l/ORlGCnybQp3aBOH9qf5tDk=";
+  meta = {
+    homepage = "";
+    description = "Redirect all Twitter/X links to use the Nitter instance xcancel.com";
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.all;
+  };
+}
