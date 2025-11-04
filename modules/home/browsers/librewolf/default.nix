3911be9eb69b10e687b11cec02d3c8a97f0472cf
Théo Bori <theo1.bori@epitech.eu>
2025-11-04 03:49:41 +0100

    :lock: Hardened librewolf

--- modules/home/browsers/librewolf/default.nix
+++ modules/home/browsers/librewolf/default.nix
@@ -19,6 +19,17 @@ in
     stylix.targets.librewolf.profileNames = [ config.${namespace}.user.name ];
     programs.librewolf = {
       enable = true;
+      settings = {
+        "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
+        "cookiebanners.service.mode" = 2; # Block cookie banners
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.fingerprintingProtection" = true;
+        "privacy.resistFingerprinting" = true;
+        "privacy.trackingprotection.emailtracking.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+      };
     };
   };
 }
