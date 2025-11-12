dc0412644d1e4eadc355b8574a82bf977e3b609f
Théo Bori <theobori@disroot.org>
2025-11-12 22:19:40 +0100

    :arrow_up: alerte-sur-les-sites-genai: 2.3.1 -> 2.5.5

--- packages/alerte-sur-les-sites-genai/default.nix
+++ packages/alerte-sur-les-sites-genai/default.nix
@@ -1,10 +1,10 @@
 { lib, pkgs, ... }:
 pkgs.nur.repos.rycee.firefox-addons.buildFirefoxXpiAddon {
   pname = "alerte-sur-les-sites-genai";
-  version = "2.3.1";
+  version = "2.5.5";
   addonId = "sebastien@gavois.fr";
-  url = "https://addons.mozilla.org/firefox/downloads/file/4574241/alerte_sur_les_sites_genai-2.3.1.xpi";
-  sha256 = "sha256-WAJSPXOTLNPlEFM7q07L5sQEgnuf5NFkfBRi6+VtZO0=";
+  url = "https://addons.mozilla.org/firefox/downloads/file/4618291/alerte_sur_les_sites_genai-2.5.5.xpi";
+  sha256 = "sha256-ZFkfEDIEhseJOpbRXCtZFbqUU8fBizLrtcA/W6k7L90=";
   meta = {
     homepage = "https://next.ink";
     description = "Displays an alert for sites generated in whole or in part by AI";
