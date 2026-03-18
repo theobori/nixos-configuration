ce7c8c7fa2913bb26816a6093592cecb0925a8d6
Théo Bori <theobori@disroot.org>
2026-03-18 14:55:02 +0100

    Removed deltachat from the `social` home-manager role

--- modules/home/roles/social/default.nix
+++ modules/home/roles/social/default.nix
@@ -19,7 +19,6 @@ in
     ${namespace} = {
       messages = {
         discord = enabled;
-        deltachat = enabled;
         dino = enabled;
         element-desktop = enabled;
         pidgin = enabled;
