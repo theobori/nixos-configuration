074df2c87e9dcab1cc2d66fd1a12204572580474
Théo Bori <theobori@disroot.org>
2026-04-29 20:48:54 +0200

    ttv-lol-pro: 2.3.10 -> 2.6.1

--- packages/ttv-lol-pro/default.nix
+++ packages/ttv-lol-pro/default.nix
@@ -1,10 +1,10 @@
 { lib, pkgs, ... }:
 pkgs.nur.repos.rycee.firefox-addons.buildFirefoxXpiAddon rec {
   pname = "ttv-lol-pro";
-  version = "2.3.10";
+  version = "2.6.1";
   addonId = "{76ef94a4-e3d0-4c6f-961a-d38a429a332b}";
-  url = "https://addons.mozilla.org/firefox/downloads/file/4357094/ttv_lol_pro-${version}.xpi";
-  sha256 = "sha256-szzQzU41IVIO8jOyEXkij9reyhaDWayrTCS7saFm/No=";
+  url = "https://addons.mozilla.org/firefox/downloads/file/4638568/ttv_lol_pro-${version}.xpi";
+  sha256 = "sha256-gutff9ZQZxnoYZe5c95ypfTo76s6t+sS6CMxfkxU+vM=";
   meta = {
     homepage = "";
     description = "";
